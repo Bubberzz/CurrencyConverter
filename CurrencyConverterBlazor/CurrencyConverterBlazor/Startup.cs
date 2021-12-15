3eaffc486e5c5a75ebf2b5c674f8a807355bb885
Stan Bubbers <stan.bubbers@live.co.uk>
2021-12-15 02:14:52 +0000

    Add: MudBlazor framework

--- CurrencyConverterBlazor/CurrencyConverterBlazor/Startup.cs
+++ CurrencyConverterBlazor/CurrencyConverterBlazor/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor.Services;
+
 
 namespace CurrencyConverterBlazor
 {
@@ -19,8 +21,10 @@ namespace CurrencyConverterBlazor
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMudServices();
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
