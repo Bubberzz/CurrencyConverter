96bd87205d0a19d9787684b270bfbea5bc869667
Stan Bubbers <stan.bubbers@live.co.uk>
2021-12-15 01:58:27 +0000

    Update: project name

--- CurrencyConverter/CurrencyConverterAPI/Startup.cs
+++ CurrencyConverter/CurrencyConverterAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace CurrencyConverter
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "CurrencyConverter", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "CurrencyConverterAPI", Version = "v1"});
             });
         }
 
@@ -49,7 +49,7 @@ namespace CurrencyConverter
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyConverter v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyConverterAPI v1"));
             }
 
             app.UseHttpsRedirection();
