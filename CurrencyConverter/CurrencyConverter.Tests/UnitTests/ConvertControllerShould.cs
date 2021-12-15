02ebb8791567a3fc42ba6247937adee80f8511bb
Stan Bubbers <stan.bubbers@live.co.uk>
2021-12-15 01:08:19 +0000

    Update: controller and unit tests

--- CurrencyConverter/CurrencyConverter.Tests/UnitTests/ConvertControllerShould.cs
+++ CurrencyConverter/CurrencyConverter.Tests/UnitTests/ConvertControllerShould.cs
@@ -1,23 +1,38 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CurrencyConverter.Controllers;
+using CurrencyConverter.Interfaces;
+using CurrencyConverter.Services;
+using CurrencyConverter.Tests.SetupHelper;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 namespace CurrencyConverter.Tests.UnitTests
 {
     public class ConvertControllerShould
     {
+        private readonly IConversionService _conversionService;
+        private readonly ICurrencyService _currencyService;
+        private readonly Mock<IConversionRepository> _mockRepository;
+
+        public ConvertControllerShould()
+        {
+            _mockRepository = new Mock<IConversionRepository>();
+            _conversionService = new ConversionService(_mockRepository.Object);
+            _currencyService = new CurrencyService();
+        }
         [Fact]
-        public async Task ConvertControllerShould_ReturnCurrencies_WhenGetCurrenciesAsyncIsCalled()
+        public async Task ConvertControllerShould_ReturnCurrencies_WhenGetCurrenciesIsCalled()
         {
             // Arrange
-            var controller = new ConvertController();
+            var controller = new ConvertController(_conversionService, _currencyService);
             
             // Act
-            var result = await GetCurrencies();
+            var result = await controller.GetCurrencies();
 
             // Assert
+            if (result.Result is OkObjectResult resultObject) Assert.Equal(_currencyService.GetCurrenciesToList(), resultObject.Value);
             Assert.IsAssignableFrom<OkObjectResult>(result.Result);
             Assert.IsType<ActionResult<List<string>>>(result);
         }
@@ -27,7 +42,8 @@ namespace CurrencyConverter.Tests.UnitTests
         public async Task ConvertControllerShould_ConvertCurrencies_WhenGetConversionAsyncIsCalled(string currencyFrom, string currencyTo, int amount)
         {
             // Arrange
-            var controller = new ConvertController();
+            _mockRepository.Setup(x => x.GetExchangeRatesAsync()).ReturnsAsync(GenerateData.GetExchangeRates());
+            var controller = new ConvertController(_conversionService, _currencyService);
             
             // Act
             var result = await controller.GetConversionAsync(currencyFrom, currencyTo, amount);
@@ -36,6 +52,7 @@ namespace CurrencyConverter.Tests.UnitTests
             if (result.Result is OkObjectResult resultObject) Assert.Equal(1.1330124M, resultObject.Value);
             Assert.IsAssignableFrom<OkObjectResult>(result.Result);
             Assert.IsType<ActionResult<decimal>>(result);
+            _mockRepository.Verify(mock => mock.GetExchangeRatesAsync(), Times.Once);
         }
     }
 }
--- CurrencyConverter/CurrencyConverter/Controllers/ConvertController.cs
+++ CurrencyConverter/CurrencyConverter/Controllers/ConvertController.cs
@@ -1,7 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CurrencyConverter.Entities;
 using CurrencyConverter.Enums;
+using CurrencyConverter.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CurrencyConverter.Controllers
@@ -10,22 +11,30 @@ namespace CurrencyConverter.Controllers
     [ApiController]
     public class ConvertController : ControllerBase
     {
+        private readonly IConversionService _conversionService;
+        private readonly ICurrencyService _currencyService;
+
+        public ConvertController(IConversionService conversionService, ICurrencyService currencyService)
+        {
+            _conversionService = conversionService;
+            _currencyService = currencyService;
+        }
+
         [Route("/convert")]
         [HttpGet]
         public async Task<ActionResult<decimal>> GetConversionAsync(string currencyFrom, string currencyTo, int amount)
         {
-            var exchangeRates = new ExchangeRate()
-            {
-                ExchangeRateId = 1
-            };
+            if (!(Enum.TryParse(currencyFrom, out Currencies from) & Enum.TryParse(currencyTo, out Currencies to)))
+                return StatusCode(500);
+            var exchangeRates = await _conversionService.Convert(@from, to, amount).ConfigureAwait(false);
             return Ok(exchangeRates);
         }
         
         [Route("/getcurrencies")]
         [HttpGet]
-        public async Task<ActionResult<List<string>>> GetCurrenciesAsync()
+        public async Task<ActionResult<List<string>>> GetCurrencies()
         {
-            return Ok(new Currencies());
+            return Ok(await Task.Run(() => _currencyService.GetCurrenciesToList()).ConfigureAwait(false));
         }
     }
 }
